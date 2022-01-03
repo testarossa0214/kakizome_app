9a711d16eaf94b5da7e5ba2471e1306385ee6aab
ruru <soundlight@live.jp>
2022-01-04 01:25:22 +0900

    ユーザー登録完了

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,6 @@
 class TweetsController < ApplicationController
     before_action :set_tweet, only: [:edit, :show]
+    before_action :move_to_index, except: [:index, :show]
 
     def index
         @tweets = Tweet.all
@@ -38,4 +39,9 @@ class TweetsController < ApplicationController
         @tweet = Tweet.find(params[:id])
     end
 
+    def move_to_index
+        unless user_signed_in?
+            redirect_to action: :index
+        end
+    end
 end
